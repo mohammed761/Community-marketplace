d1c0bbf88813c5b8a7a1d3002190504e42e15c9c
Mohammed Sgier <m7mdsgier1@gmail.com>
2025-09-30 12:24:04 +0300

    first push

---
+++ src/test/java/com/community/marketplace/CommunityMarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.community.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityMarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
